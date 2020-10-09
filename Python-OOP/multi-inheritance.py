bcb72977153a321156037cd528164d432838c6b0
Glezzy <gsg@pdx.edu>
2020-10-09 08:17:20 -0600

    Added attributes

--- Python-OOP/multi-inheritance.py
+++ Python-OOP/multi-inheritance.py
@@ -3,19 +3,27 @@
 #   Set attributes with default values
 
 class Class1: 
-    def m(self): 
+    def m(self):
+        class1att = 'random1'
+        class1att2 = 'random2'
         print("In Class1")  
         
 class Class2(Class1): 
-    def m(self): 
+    def m(self):
+        class2att = 'random1'
+        class2att2 = 'random2'
         print("In Class2") 
   
 class Class3(Class1): 
-    def m(self): 
-         print("In Class3")      
+    def m(self):
+        class3att = 'random1'
+        class3att2 = 'random2'
+        print("In Class3")      
       
 class Class4(Class2, Class3): 
-    def m(self): 
+    def m(self):
+        class4att = 'random1'
+        class4att2 = 'random2'
         print("In Class4")    
   
 obj = Class4() 
