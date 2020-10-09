858b9e4f5c330ab878e52f22ba88ba908e670ba3
Glezzy <gsg@pdx.edu>
2020-10-09 08:35:30 -0600

    Assignment 222 Encapsulation

--- Python-OOP/Encapsulation.py
+++ Python-OOP/Encapsulation.py
@@ -8,15 +8,15 @@ class Employee:
         # protected variable
         self.name = 'Unknown'
         self.email = 'Unknown'
-        self._salary = 55000 # protected attribute
-        self.__pin = 0123 # private attribute
+        self._salary = 900 # protected attribute
+        self.__pin = 9999 # private attribute
 
     # Private method
     def __getPin(self):
         print("Your private pin number: {}", self.__pin)
 
 # Instantiate a class object
-emp1 = Employee("Bob Smith", "bobsmith@gmail.com", 60000, 4554)
+emp1 = Employee("Bob Smith", "bobsmith@gmail.com", 22000, 1612)
 emp1.name
 emp1.email
 emp1._salary
