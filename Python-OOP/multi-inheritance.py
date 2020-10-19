7de8a07b640207b2c9d9a4dc7d8f114831fe3264
Glezzy <gsg@pdx.edu>
2020-10-19 10:26:22 -0600

    Simplified assignment

--- Python-OOP/multi-inheritance.py
+++ Python-OOP/multi-inheritance.py
@@ -3,42 +3,17 @@
 #   Set attributes with default values
 
 
-class Animals: 
-      
-    # Initializing constructor 
-    def __init__(self): 
-        self.legs = 4
-        self.domestic = True
-        self.tail = True
-        self.mammals = True
-      
-    def isMammal(self): 
-        if self.isMammal: 
-            print("It is a mammal.") 
-      
-    def isDomestic(self): 
-        if self.mammals: 
-            print("It is a domestic animal.") 
-      
-class Dogs(Animals): 
-    def __init__(self): 
-        super().__init__() 
-  
-    def isMammal(self): 
-        super().isMammal() 
-  
-class Cat(Animals): 
-    def __init__(self): 
-        super().__init__() 
-  
-    def hasTailandLegs(self): 
-        if self.tail and self.legs == 4: 
-            print("Has legs and tail") 
-  
-# Dirver code 
-Hebe = Dogs() 
-Hebe.isMammal() 
-Baxter = Cat() 
-Baxter.hasTailandLegs() 
+class Animal:
+    has_fur = 'true'
+    number_of_legs = 4
+
+class Cat(Animal):
+    claw_sharpness = 86 # on a 0-100 scale
+    number_of_lives = 9 # normal value is 1
+
+class Turtle(Animal):
+    breath_underwater = 'true'
+    animal_type = 'Reptile'
+    
     
     
